83c0cc40a7da90cfa990cf5b3ebc2880aeb3957e
Rafaelmb26 <rafamb14@hotmail.com>
2017-09-27 23:32:50 -0300

    Inicio criação tela Login

--- Projeto/Rikifoki/src/ViewLogin.java
+++ Projeto/Rikifoki/src/ViewLogin.java
@@ -4,10 +4,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.Dimension;
+import java.awt.Window.Type;
 
 public class ViewLogin extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -29,12 +39,40 @@ public class ViewLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public ViewLogin() {
+		setResizable(false);
+		setSize(new Dimension(200, 200));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblRikifoki = new JLabel("RIKIFOKI");
+		lblRikifoki.setFont(new Font("Showcard Gothic", Font.PLAIN, 30));
+		lblRikifoki.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRikifoki.setBounds(0, 76, 270, 80);
+		contentPane.add(lblRikifoki);
+		
+		JLabel lblNewLabel = new JLabel("Usu\u00E1rio");
+		lblNewLabel.setBounds(10, 167, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Senha");
+		lblNewLabel_1.setBounds(10, 192, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(66, 164, 149, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(66, 189, 149, 20);
+		contentPane.add(passwordField);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setBounds(76, 220, 89, 23);
+		contentPane.add(btnEntrar);
 	}
-
 }
